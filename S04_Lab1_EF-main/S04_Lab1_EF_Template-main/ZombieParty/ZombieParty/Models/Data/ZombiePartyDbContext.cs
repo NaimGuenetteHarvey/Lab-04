730c122a41f8bd576dfc81da3a043b266d5ed6a6
Naïm Guénette-Harvey <2360188@cegepmontpetit.ca>
2025-09-05 14:20:12 -0400

    DÉBUT CRÉATION DE L'APPLICATION DBCONTEXT

---
+++ S04_Lab1_EF-main/S04_Lab1_EF_Template-main/ZombieParty/ZombieParty/Models/Data/ZombiePartyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ZombieParty.Models.Data
+{
+    public class ZombiePartyDbContext : DbContext
+    {
+        public ZombiePartyDbContext(DbContextOptions<ZombiePartyDbContext> options) :base(options)
+    {
+
+    }
+}
+    
+}
